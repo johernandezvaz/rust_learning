f6fba2c4bcf21056d959b9a4d1be82d8ee8de916
johernandezvaz <johernandezvaz@gmail.com>
2024-10-04 09:39:41 -0600

    hello cargo updates

---
+++ hello_cargo/target/debug/deps/hello_cargo-6a9d1e1652a4f90c.d
@@ -0,0 +1,5 @@
+C:\Users\joher\Documents\codigos\rust_learning\rust_learning\hello_cargo\target\debug\deps\libhello_cargo-6a9d1e1652a4f90c.rmeta: src/main.rs
+
+C:\Users\joher\Documents\codigos\rust_learning\rust_learning\hello_cargo\target\debug\deps\hello_cargo-6a9d1e1652a4f90c.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo-bf96efc5cdddcbb5.d
@@ -0,0 +1,5 @@
+C:\Users\joher\Documents\codigos\rust_learning\rust_learning\hello_cargo\target\debug\deps\libhello_cargo-bf96efc5cdddcbb5.rmeta: src/main.rs
+
+C:\Users\joher\Documents\codigos\rust_learning\rust_learning\hello_cargo\target\debug\deps\hello_cargo-bf96efc5cdddcbb5.d: src/main.rs
+
+src/main.rs:
